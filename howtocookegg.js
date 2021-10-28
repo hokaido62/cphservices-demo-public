d9bcd18fec371d936537595114ee8d3984a65a33
hokaido62 <43979188+hokaido62@users.noreply.github.com>
2021-10-28 10:57:24 +0800

    Update howtocookegg.js
    Added cracking the egg

--- howtocookegg.js
+++ howtocookegg.js
@@ -7,3 +7,5 @@ console.log("Boil water into it");
 console.log("Put the egg in the boiling water for 5 minutes");
 
 console.log("Put the egg in an iced water for 1 minute");
+
+console.log("Crack the egg");
