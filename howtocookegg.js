3560d3c69417acc055afc1c49c19231431bc1f82
hokaido62 <43979188+hokaido62@users.noreply.github.com>
2021-10-28 10:56:33 +0800

    Create howtocookegg.js

---
+++ howtocookegg.js
@@ -0,0 +1,9 @@
+console.log("This is my first recipe");
+
+console.log("Prepare the pot");
+
+console.log("Boil water into it");
+
+console.log("Put the egg in the boiling water for 5 minutes");
+
+console.log("Put the egg in an iced water for 1 minute");
